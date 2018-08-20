e15db9de498bb7a7f424a67d51fe3a6e56454ae4
jbg77 <jbgaume@gmail.com>
2018-08-20 20:37:37 +0200

    Update start.sh

--- rtorrent-flood-openvpn/scripts/start.sh
+++ rtorrent-flood-openvpn/scripts/start.sh
@@ -23,14 +23,9 @@ fix_permissions() {
 }
 
 session_openvpn() {
-    if [ -z "$OVPN_CONFIG" ]; then
-        echo "NO VPN FILE SPECIFIED\nTORRENTING WITHOUT VPN"
-        tail -f /dev/null
-    elif [ -z "$OVPN_LOGIN" ] || [ -z "$OVPN_PASSWORD" ]; then
-        screen -S openvpn -m openvpn --config $OVPN_CONFIG
-    else
-        screen -S openvpn -m expect /scripts/connect_vpn.sh
-    fi
+    
+        screen -d openvpn client.ovpn
+    
 }
 
 start_sessions() {
